a887ebdab66cf92d13901f53a9b61ff1869036b7
Coqueiro <kartgarcia@gmail.com>
2018-11-08 00:49:59 -0200

    first commit

---
+++ build/precache-manifest.bfc48f2204b7cb3dedb36b92aec1a252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76516adfe034fbc189d9",
+    "url": "/static/js/main.76516adf.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "76516adfe034fbc189d9",
+    "url": "/static/css/main.378f159f.chunk.css"
+  },
+  {
+    "revision": "5d4e64ba8c1088505b1e21ca4bd2081e",
+    "url": "/index.html"
+  }
+];
